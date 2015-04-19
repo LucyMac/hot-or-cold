2c66300f90e36815e34b54319c8d9359bd6cfdd5
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-04-19 10:35:03 +1000

    inital commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +11,55 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+//getting the computer to generate a number 
+var newGame = function () {
+	var computerChoice = Math.random() * 100 {
+				return Math.round(computerChoice) //could this be Math.random.round? 
+			}
+		}
+
+//assigning what to user inputs to the variable userInput
+var userInput = $("input").val();
+
+//comparing userInput to computerChoice on clicking the button
+$("guessButton")
+	.click(
+		feedback(userInput, computerChoice) {        //I'm calling the feedback function before I've defined it. Is this a problem?
+			var feedback = function(userInput, computerChoice) {
+				if (userInput === computerChoice) {
+					var node = document.createElement("li");         
+					var textnode = document.createTextNode("nice one! The number was" + userInput);
+					node.appendChild(textnode);
+					document.getElementById("guessList").appendChild(node);
+				}
+				else if ((userInput - computerChoice) > 15 || -15) {
+					var node = document.createElement("li");         
+					var textnode = document.createTextNode("cold");
+					node.appendChild(textnode);
+					document.getElementById("guessList").appendChild(node);
+				}
+				else if ((userInput - computerChoice) > 10 || -10) {
+					var node = document.createElement("li");         
+					var textnode = document.createTextNode("hotter");
+					node.appendChild(textnode);
+					document.getElementById("guessList").appendChild(node);
+				}
+				else if ((userInput - computerChoice) > 5 || -5) {
+					var node = document.createElement("li");         
+					var textnode = document.createTextNode("hot");
+					node.appendChild(textnode);
+					document.getElementById("guessList").appendChild(node);
+				}
+				else if ((userInput - computerChoice) > 1 || -1) {
+					var node = document.createElement("li");         
+					var textnode = document.createTextNode("burning");
+					node.appendChild(textnode);
+					document.getElementById("guessList").appendChild(node);
+				}
+			};
+	
+	});
+	
 });
 
 
