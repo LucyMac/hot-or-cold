538dcc9e6563dc694f54cd8d90933ccf9fbb5769
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-04-21 12:37:09 +1000

    major js changes

--- js/app.js
+++ js/app.js
@@ -11,62 +11,66 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+var computerChoice = "";    //we declare computerChoice outside of the newGame function so it can have a global scope. 
+							//This way we can call it in subsequent functions: feedback and userAttempt. 
+							//this means we do not use the word 'var' in newGame, we just say 'computerChoice' to avoid redefining its scope. 
+
 //new game generates new computer number
 var newGame = function () {
-	var computerChoice = Math.random() * 100 {     //getting the computer to generate a number 
-		return Math.round(computerChoice);    //could this be Math.random.round? 
-	}
+	computerChoice = Math.floor((Math.random() * 100) + 1); //generates a number between 1 and 100 and rounds it down to eliminate decimals. 
+	console.log(computerChoice);
 	userAttempt();
 }
 
+//creating feedback() function to compare user input with computer-generated number. 
+//This is only executed when user makes a guess, see userAttempt() function below. 
+var feedback = function(userInput, computerChoice) {
+	console.log(userInput);
+	var difference = Math.abs(userInput - computerChoice);
+	if (userInput === computerChoice) {
+		var node = document.createElement("li");         
+		var textnode = document.createTextNode("nice one! The number was" + userInput);
+		node.appendChild(textnode);
+		document.getElementById("guessList").appendChild(node);
+	}
+	else if (difference > 15) {
+		var node = document.createElement("li");         
+		var textnode = document.createTextNode("cold");
+		node.appendChild(textnode);
+		document.getElementById("guessList").appendChild(node);
+	}
+	else if (difference > 10) {
+		var node = document.createElement("li");         
+		var textnode = document.createTextNode("hotter");
+		node.appendChild(textnode);
+		document.getElementById("guessList").appendChild(node);
+	}
+	else if (difference > 5) {
+		var node = document.createElement("li");         
+		var textnode = document.createTextNode("hot");
+		node.appendChild(textnode);
+		document.getElementById("guessList").appendChild(node);
+	}
+	else if (difference > 1) {
+		var node = document.createElement("li");         
+		var textnode = document.createTextNode("burning");
+		node.appendChild(textnode);
+		document.getElementById("guessList").appendChild(node);
+	}
+};
+
 //each time user submits new guess, compare this guess with computer's choice
 var userAttempt = function () {
-	document.getElementById("guessButton").onclick;
-    var userNumber = document.getElementById("userGuess").val("");
+	document.getElementById("guessButton").addEventListener("click", function(event){
+    	event.preventDefault();
+    	var userNumber = parseInt(document.getElementById("userGuess").value); //parseInt turns string into number
+    	feedback(userNumber, computerChoice);
+	});	
 }
 
-//retrieving the value
-var userInput = $("input").val();
+//Executing newGame once on page load. 
+//Note: newGame also runs when ".clearfix li.new" is clicked. See index.html
+newGame();
 
-//comparing userInput to computerChoice on clicking the button
-$("guessButton")
-	.click(
-		feedback(userInput, computerChoice) {        //I'm calling the feedback function before I've defined it. Is this a problem?
-			var feedback = function(userInput, computerChoice) {
-				if (userInput === computerChoice) {
-					var node = document.createElement("li");         
-					var textnode = document.createTextNode("nice one! The number was" + userInput);
-					node.appendChild(textnode);
-					document.getElementById("guessList").appendChild(node);
-				}
-				else if ((userInput - computerChoice) > 15 || -15) {
-					var node = document.createElement("li");         
-					var textnode = document.createTextNode("cold");
-					node.appendChild(textnode);
-					document.getElementById("guessList").appendChild(node);
-				}
-				else if ((userInput - computerChoice) > 10 || -10) {
-					var node = document.createElement("li");         
-					var textnode = document.createTextNode("hotter");
-					node.appendChild(textnode);
-					document.getElementById("guessList").appendChild(node);
-				}
-				else if ((userInput - computerChoice) > 5 || -5) {
-					var node = document.createElement("li");         
-					var textnode = document.createTextNode("hot");
-					node.appendChild(textnode);
-					document.getElementById("guessList").appendChild(node);
-				}
-				else if ((userInput - computerChoice) > 1 || -1) {
-					var node = document.createElement("li");         
-					var textnode = document.createTextNode("burning");
-					node.appendChild(textnode);
-					document.getElementById("guessList").appendChild(node);
-				}
-			};
-	
-	});
-	
 });
-
 
